bddb4af1fa89c78fe3fd3dfa1ff6c48234d8f4f7
fedotick <feodortcaciuc2004@gmail.com>
2024-01-22 23:19:49 +0200

    Initial Application

---
+++ VPLaboratoryWork1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace VPLaboratoryWork1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AmericanAvocet = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.AmericanKestrel = new System.Windows.Forms.Label();
+            this.AtlanticPuffin = new System.Windows.Forms.Label();
+            this.BaltimoreOriole = new System.Windows.Forms.Label();
+            this.CrownedCrane = new System.Windows.Forms.Label();
+            this.GouldianFinch = new System.Windows.Forms.Label();
+            this.NorthernCardinal = new System.Windows.Forms.Label();
+            this.Peacock = new System.Windows.Forms.Label();
+            this.Kingfisher = new System.Windows.Forms.Label();
+            this.RedMacawParrot = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AmericanAvocet
+            // 
+            this.AmericanAvocet.AutoSize = true;
+            this.AmericanAvocet.BackColor = System.Drawing.Color.Transparent;
+            this.AmericanAvocet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AmericanAvocet.Location = new System.Drawing.Point(100, 82);
+            this.AmericanAvocet.Name = "AmericanAvocet";
+            this.AmericanAvocet.Size = new System.Drawing.Size(88, 13);
+            this.AmericanAvocet.TabIndex = 0;
+            this.AmericanAvocet.Text = "American Avocet";
+            this.AmericanAvocet.Click += new System.EventHandler(this.AmericanAvocet_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(280, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AmericanKestrel
+            // 
+            this.AmericanKestrel.AutoSize = true;
+            this.AmericanKestrel.BackColor = System.Drawing.Color.Transparent;
+            this.AmericanKestrel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AmericanKestrel.Location = new System.Drawing.Point(100, 102);
+            this.AmericanKestrel.Name = "AmericanKestrel";
+            this.AmericanKestrel.Size = new System.Drawing.Size(86, 13);
+            this.AmericanKestrel.TabIndex = 2;
+            this.AmericanKestrel.Text = "American Kestrel";
+            this.AmericanKestrel.Click += new System.EventHandler(this.AmericanKestrel_Click);
+            // 
+            // AtlanticPuffin
+            // 
+            this.AtlanticPuffin.AutoSize = true;
+            this.AtlanticPuffin.BackColor = System.Drawing.Color.Transparent;
+            this.AtlanticPuffin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AtlanticPuffin.Location = new System.Drawing.Point(100, 122);
+            this.AtlanticPuffin.Name = "AtlanticPuffin";
+            this.AtlanticPuffin.Size = new System.Drawing.Size(72, 13);
+            this.AtlanticPuffin.TabIndex = 2;
+            this.AtlanticPuffin.Text = "Atlantic Puffin";
+            this.AtlanticPuffin.Click += new System.EventHandler(this.AtlanticPuffin_Click);
+            // 
+            // BaltimoreOriole
+            // 
+            this.BaltimoreOriole.AutoSize = true;
+            this.BaltimoreOriole.BackColor = System.Drawing.Color.Transparent;
+            this.BaltimoreOriole.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BaltimoreOriole.Location = new System.Drawing.Point(100, 142);
+            this.BaltimoreOriole.Name = "BaltimoreOriole";
+            this.BaltimoreOriole.Size = new System.Drawing.Size(80, 13);
+            this.BaltimoreOriole.TabIndex = 0;
+            this.BaltimoreOriole.Text = "Baltimore Oriole";
+            this.BaltimoreOriole.Click += new System.EventHandler(this.BaltimoreOriole_Click);
+            // 
+            // CrownedCrane
+            // 
+            this.CrownedCrane.AutoSize = true;
+            this.CrownedCrane.BackColor = System.Drawing.Color.Transparent;
+            this.CrownedCrane.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CrownedCrane.Location = new System.Drawing.Point(100, 162);
+            this.CrownedCrane.Name = "CrownedCrane";
+            this.CrownedCrane.Size = new System.Drawing.Size(80, 13);
+            this.CrownedCrane.TabIndex = 2;
+            this.CrownedCrane.Text = "Crowned Crane";
+            this.CrownedCrane.Click += new System.EventHandler(this.CrownedCrane_Click);
+            // 
+            // GouldianFinch
+            // 
+            this.GouldianFinch.AutoSize = true;
+            this.GouldianFinch.BackColor = System.Drawing.Color.Transparent;
+            this.GouldianFinch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.GouldianFinch.Location = new System.Drawing.Point(100, 182);
+            this.GouldianFinch.Name = "GouldianFinch";
+            this.GouldianFinch.Size = new System.Drawing.Size(78, 13);
+            this.GouldianFinch.TabIndex = 2;
+            this.GouldianFinch.Text = "Gouldian Finch";
+            this.GouldianFinch.Click += new System.EventHandler(this.GouldianFinch_Click);
+            // 
+            // NorthernCardinal
+            // 
+            this.NorthernCardinal.AutoSize = true;
+            this.NorthernCardinal.BackColor = System.Drawing.Color.Transparent;
+            this.NorthernCardinal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NorthernCardinal.Location = new System.Drawing.Point(100, 222);
+            this.NorthernCardinal.Name = "NorthernCardinal";
+            this.NorthernCardinal.Size = new System.Drawing.Size(89, 13);
+            this.NorthernCardinal.TabIndex = 0;
+            this.NorthernCardinal.Text = "Northern Cardinal";
+            this.NorthernCardinal.Click += new System.EventHandler(this.NorthernCardinal_Click);
+            // 
+            // Peacock
+            // 
+            this.Peacock.AutoSize = true;
+            this.Peacock.BackColor = System.Drawing.Color.Transparent;
+            this.Peacock.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Peacock.Location = new System.Drawing.Point(100, 242);
+            this.Peacock.Name = "Peacock";
+            this.Peacock.Size = new System.Drawing.Size(50, 13);
+            this.Peacock.TabIndex = 2;
+            this.Peacock.Text = "Peacock";
+            this.Peacock.Click += new System.EventHandler(this.Peacock_Click);
+            // 
+            // Kingfisher
+            // 
+            this.Kingfisher.AutoSize = true;
+            this.Kingfisher.BackColor = System.Drawing.Color.Transparent;
+            this.Kingfisher.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Kingfisher.Location = new System.Drawing.Point(100, 202);
+            this.Kingfisher.Name = "Kingfisher";
+            this.Kingfisher.Size = new System.Drawing.Size(53, 13);
+            this.Kingfisher.TabIndex = 2;
+            this.Kingfisher.Text = "Kingfisher";
+            this.Kingfisher.Click += new System.EventHandler(this.Kingfisher_Click);
+            // 
+            // RedMacawParrot
+            // 
+            this.RedMacawParrot.AutoSize = true;
+            this.RedMacawParrot.BackColor = System.Drawing.Color.Transparent;
+            this.RedMacawParrot.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RedMacawParrot.Location = new System.Drawing.Point(100, 262);
+            this.RedMacawParrot.Name = "RedMacawParrot";
+            this.RedMacawParrot.Size = new System.Drawing.Size(96, 13);
+            this.RedMacawParrot.TabIndex = 2;
+            this.RedMacawParrot.Text = "Red Macaw Parrot";
+            this.RedMacawParrot.Click += new System.EventHandler(this.RedMacawParrot_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Transparent;
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitButton.FlatAppearance.BorderSize = 0;
+            this.ExitButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.ExitButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.ForeColor = System.Drawing.Color.White;
+            this.ExitButton.Location = new System.Drawing.Point(354, 310);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.RedMacawParrot);
+            this.Controls.Add(this.GouldianFinch);
+            this.Controls.Add(this.Kingfisher);
+            this.Controls.Add(this.Peacock);
+            this.Controls.Add(this.AtlanticPuffin);
+            this.Controls.Add(this.CrownedCrane);
+            this.Controls.Add(this.NorthernCardinal);
+            this.Controls.Add(this.AmericanKestrel);
+            this.Controls.Add(this.BaltimoreOriole);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.AmericanAvocet);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Birds";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AmericanAvocet;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label AmericanKestrel;
+        private System.Windows.Forms.Label AtlanticPuffin;
+        private System.Windows.Forms.Label BaltimoreOriole;
+        private System.Windows.Forms.Label CrownedCrane;
+        private System.Windows.Forms.Label GouldianFinch;
+        private System.Windows.Forms.Label NorthernCardinal;
+        private System.Windows.Forms.Label Peacock;
+        private System.Windows.Forms.Label Kingfisher;
+        private System.Windows.Forms.Label RedMacawParrot;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ VPLaboratoryWork1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VPLaboratoryWork1
+{
+    public partial class Form1 : Form
+    {
+        private float alpha = 0.9f;
+        private Label lastActiveLabel = new Label();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AmericanAvocet_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/AmericanAvocet.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            AmericanAvocet.ForeColor = Color.Gray;
+            lastActiveLabel = AmericanAvocet;
+        }
+
+        private void AmericanKestrel_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/AmericanKestrel.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            AmericanKestrel.ForeColor = Color.Gray;
+            lastActiveLabel = AmericanKestrel;
+        }
+
+        private void AtlanticPuffin_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/AtlanticPuffin.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            AtlanticPuffin.ForeColor = Color.Gray;
+            lastActiveLabel = AtlanticPuffin;
+        }
+
+        private void BaltimoreOriole_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/BaltimoreOriole.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            BaltimoreOriole.ForeColor = Color.Gray;
+            lastActiveLabel = BaltimoreOriole;
+        }
+
+        private void CrownedCrane_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/CrownedCrane.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            CrownedCrane.ForeColor = Color.Gray;
+            lastActiveLabel = CrownedCrane;
+        }
+
+        private void GouldianFinch_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/GouldianFinch.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            GouldianFinch.ForeColor = Color.Gray;
+            lastActiveLabel = GouldianFinch;
+        }
+
+        private void Kingfisher_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/Kingfisher.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            Kingfisher.ForeColor = Color.Gray;
+            lastActiveLabel = Kingfisher;
+        }
+
+        private void NorthernCardinal_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/NorthernCardinal.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            NorthernCardinal.ForeColor = Color.Gray;
+            lastActiveLabel = NorthernCardinal;
+        }
+
+        private void Peacock_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/Peacock.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            Peacock.ForeColor = Color.Gray;
+            lastActiveLabel = Peacock;
+        }
+
+        private void RedMacawParrot_Click(object sender, EventArgs e)
+        {
+            lastActiveLabel.ForeColor = Color.White;
+
+            Image image = Image.FromFile(@"..\..\Resources/Images/Birds/RedMacawParrot.jpg");
+
+            pictureBox1.Image = image;
+            this.BackgroundImage = ApplyDarkOverlay(image, alpha);
+
+            RedMacawParrot.ForeColor = Color.Gray;
+            lastActiveLabel = RedMacawParrot;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public static Bitmap ApplyDarkOverlay(Image originalImage, float overlayAlpha)
+        {
+            Bitmap resultImage = new Bitmap(originalImage);
+
+            using (Graphics g = Graphics.FromImage(resultImage))
+            {
+                using (Brush brush = new SolidBrush(Color.FromArgb((int)(255 * overlayAlpha), Color.Black)))
+                {
+                    g.FillRectangle(brush, 0, 0, resultImage.Width, resultImage.Height);
+                }
+            }
+
+            return resultImage;
+        }
+    }
+}
